6ef1e1ef05fb89d7c7b62c64ace76edb28114dfe
Aidan <38162@ma-web.nl>
2023-11-20 15:12:38 +0100

    op2 1ste aanpassing

--- 01A/app.js
+++ 01A/app.js
@@ -4,6 +4,13 @@ class App
     {
         console.log("hello world!");
         //code hier
+        let appName = "app 👍"
+        let verNum = "0.1"
+        let verDate = newdate("september 20 2023")
+        let author = "Aidan Jones"
+        let copyright = "none cuz i'm cool"
+        let distributor = "dunno"
+        let darkmode = "false"
     }
 }
 
